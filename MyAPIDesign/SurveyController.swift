9c0ba79190c31b11315b620562d0648297c6fa9b
Michael Mecham <myke.mecham@gmail.com>
2016-07-14 11:33:10 -0600

    implented put method in SurveyController

--- MyAPIDesign/SurveyController.swift
+++ MyAPIDesign/SurveyController.swift
@@ -13,4 +13,19 @@ class SurveyController {
     static let baseURL = NSURL(string: "https://mykeapidesign-a7954.firebaseio.com/api/v1")
     static let getterEndpoint = baseURL?.URLByAppendingPathExtension("json")
     
+    static func putSurveyIntoAPI(name: String, response: String) {
+        let survey = Survey(name: name, response: response)
+        guard let url = survey.endpoint else { return }
+        NetworkController.performRequestForURL(url, httpMethod: .Put, body: survey.jsonData) { (data, error) in
+            let responseDataString = NSString(data: data!, encoding: NSUTF8StringEncoding) ?? ""
+            if error != nil {
+                print("Error: \(error?.localizedDescription)")
+            } else if responseDataString.containsString("error") {
+                print("Error: \(responseDataString)")
+            } else {
+                print("Successfully daved data to endpoint. \nResponse: \(responseDataString)")
+            }
+        }
+    }
+    
 }
