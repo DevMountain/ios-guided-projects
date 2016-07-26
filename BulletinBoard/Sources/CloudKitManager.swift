9af02e768d01ef7e95010151eb1d31fc910859c2
Andrew Madsen <andrew@openreelsoftware.com>
2016-07-26 11:19:15 -0600

    Greatly simplifies CloudKitManager.

--- BulletinBoard/Sources/CloudKitManager.swift
+++ BulletinBoard/Sources/CloudKitManager.swift
@@ -1,8 +1,8 @@
 //
 //  CloudKitManager.swift
-//  Timeline
+//  BulletinBoard
 //
-//  Created by Caleb Hicks on 5/27/16.
+//  Created by Andrew Madsen on 6/18/16.
 //  Copyright © 2016 DevMountain. All rights reserved.
 //
 
@@ -16,141 +16,37 @@ private let CreationDateKey = "creationDate"
 private let ModificationDateKey = "modificationDate"
 
 class CloudKitManager {
-    
-    let publicDatabase = CKContainer.defaultContainer().publicCloudDatabase
-    let privateDatabase = CKContainer.defaultContainer().privateCloudDatabase
-    
-    init() {
-        checkCloudKitAvailability()
-    }
 	
-    // MARK: - Fetch Records
+	let database = CKContainer.defaultContainer().publicCloudDatabase
 	
-    func fetchRecordsWithType(type: String, predicate: NSPredicate = NSPredicate(value: true), recordFetchedBlock: ((record: CKRecord) -> Void)?, completion: ((records: [CKRecord]?, error: NSError?) -> Void)?) {
-        
-        var fetchedRecords: [CKRecord] = []
-        
-        let predicate = predicate
-        let query = CKQuery(recordType: type, predicate: predicate)
-        let queryOperation = CKQueryOperation(query: query)
-        
-        queryOperation.recordFetchedBlock = { (fetchedRecord) -> Void in
-            
-            fetchedRecords.append(fetchedRecord)
-            
-            if let recordFetchedBlock = recordFetchedBlock {
-                recordFetchedBlock(record: fetchedRecord)
-            }
-        }
-        
-        queryOperation.queryCompletionBlock = { (queryCursor, error) -> Void in
-            
-            if let queryCursor = queryCursor {
-                // there are more results, go fetch them
-                
-                let continuedQueryOperation = CKQueryOperation(cursor: queryCursor)
-                continuedQueryOperation.recordFetchedBlock = queryOperation.recordFetchedBlock
-                continuedQueryOperation.queryCompletionBlock = queryOperation.queryCompletionBlock
-                
-                self.publicDatabase.addOperation(continuedQueryOperation)
-            } else {
-                if let completion = completion {
-                    completion(records: fetchedRecords, error: error)
-                }
-            }
-        }
-        
-        self.publicDatabase.addOperation(queryOperation)
-    }
-    
-    // MARK: - Save and Modify
+	func fetchRecordsWithType(type: String,
+	                          sortDescriptors: [NSSortDescriptor]? = nil,
+	                          completion: ([CKRecord]?, NSError?) -> Void) {
+		
+		let query = CKQuery(recordType: type, predicate: NSPredicate(value: true))
+		query.sortDescriptors = sortDescriptors
+		
+		database.performQuery(query, inZoneWithID: nil, completionHandler: completion)
+	}
 	
-    func saveRecord(record: CKRecord, completion: ((record: CKRecord?, error: NSError?) -> Void)?) {
-        
-        publicDatabase.saveRecord(record) { (record, error) in
-            
-            if let completion = completion {
-                completion(record: record, error: error)
-            }
-        }
-    }
+	func saveRecord(record: CKRecord, completion: ((NSError?) -> Void) = {_ in }) {
+		
+		database.saveRecord(record) { (_, error) in
+			completion(error)
+		}
+	}
 	
-    // MARK: - Subscriptions
-    
-    func subscribe(type: String, predicate: NSPredicate, subscriptionID: String, contentAvailable: Bool, alertBody: String? = nil, desiredKeys: [String]? = nil, options: CKSubscriptionOptions, completion: ((subscription: CKSubscription?, error: NSError?) -> Void)?) {
-        
-        let subscription = CKSubscription(recordType: type, predicate: predicate, subscriptionID: subscriptionID, options: options)
-        
-        let notificationInfo = CKNotificationInfo()
-        notificationInfo.alertBody = alertBody
-		if alertBody != nil { notificationInfo.soundName = UILocalNotificationDefaultSoundName }
-        notificationInfo.shouldSendContentAvailable = contentAvailable
-        notificationInfo.desiredKeys = desiredKeys
-        
-        subscription.notificationInfo = notificationInfo
-        
-        publicDatabase.saveSubscription(subscription) { (subscription, error) in
-            
-            if let completion = completion {
-                completion(subscription: subscription, error: error)
-            }
-        }
-    }
-	
-    // MARK: - CloudKit Permissions
-    
-    func checkCloudKitAvailability() {
-        
-        CKContainer.defaultContainer().accountStatusWithCompletionHandler() {
-            (accountStatus:CKAccountStatus, error:NSError?) -> Void in
-            
-            switch accountStatus {
-            case .Available:
-                print("CloudKit available. Initializing full sync.")
-                return
-            default:
-                self.handleCloudKitUnavailable(accountStatus, error: error)
-            }
-        }
-    }
-    
-    func handleCloudKitUnavailable(accountStatus: CKAccountStatus, error:NSError?) {
-        
-        var errorText = "Synchronization is disabled\n"
-        if let error = error {
-            print("handleCloudKitUnavailable ERROR: \(error)")
-            print("An error occured: \(error.localizedDescription)")
-            errorText += error.localizedDescription
-        }
-        
-        switch accountStatus {
-        case .Restricted:
-            errorText += "iCloud is not available due to restrictions"
-        case .NoAccount:
-            errorText += "There is no CloudKit account setup.\nYou can setup iCloud in the Settings app."
-        default:
-            break
-        }
-        
-        displayCloudKitNotAvailableError(errorText)
-    }
-    
-    func displayCloudKitNotAvailableError(errorText: String) {
-        
-        dispatch_async(dispatch_get_main_queue(),{
-            
-            let alertController = UIAlertController(title: "iCloud Synchronization Error", message: errorText, preferredStyle: .Alert)
-            
-            let dismissAction = UIAlertAction(title: "Ok", style: .Cancel, handler: nil);
-            
-            alertController.addAction(dismissAction)
-            
-            if let appDelegate = UIApplication.sharedApplication().delegate,
-                let appWindow = appDelegate.window!,
-                let rootViewController = appWindow.rootViewController {
-                rootViewController.presentViewController(alertController, animated: true, completion: nil)
-            }
-        })
-    }
-	
+	func subscribeToCreationOfRecordsWithType(type: String, completion: ((NSError?) -> Void)? = nil) {
+		let subscription = CKSubscription(recordType: Message.recordType, predicate: NSPredicate(value: true), options: .FiresOnRecordCreation)
+		let notificationInfo = CKNotificationInfo()
+		notificationInfo.alertBody = "There's a new message on the bulletin board."
+		notificationInfo.soundName = UILocalNotificationDefaultSoundName
+		subscription.notificationInfo = notificationInfo
+		database.saveSubscription(subscription) { (subscription, error) in
+			if let error = error {
+				NSLog("Error saving subscription: \(error)")
+			}
+			completion?(error)
+		}
+	}
 }
--- BulletinBoard/Sources/MessagesController.swift
+++ BulletinBoard/Sources/MessagesController.swift
@@ -8,12 +8,7 @@
 
 import Foundation
 import CloudKit
-
-#if os(OSX)
-	import Cocoa
-#else
-	import UIKit
-#endif
+import UIKit
 
 public let MessagesControllerDidRefreshNotification = "MessagesControllerDidRefreshNotification"
 
@@ -30,7 +25,7 @@ public class MessagesController: NSObject {
 	func postNewMessage(message: Message, completion: ((NSError?) -> Void)? = nil) {
 		let record = message.cloudKitRecord
 		
-		cloudKitManager.saveRecord(message.cloudKitRecord) { (record, error) in
+		cloudKitManager.saveRecord(message.cloudKitRecord) { (error) in
 			defer { completion?(error) }
 			if let error = error {
 				NSLog("Error saving \(message) to CloudKit: \(error)")
@@ -41,25 +36,25 @@ public class MessagesController: NSObject {
 	}
 	
 	func refresh(completion: ((NSError?) -> Void)? = nil) {
-
-		cloudKitManager.fetchRecordsWithType(Message.recordType, recordFetchedBlock: nil) { (records, error) in
+		let sortDescriptors = [NSSortDescriptor(key: Message.dateKey, ascending: false)]
+		cloudKitManager.fetchRecordsWithType(Message.recordType, sortDescriptors: sortDescriptors) {
+			(records, error) in
+			
 			defer { completion?(error) }
+			
 			if let error = error {
-				NSLog("Error fetching from CloudKit: \(error)")
+				NSLog("Error fetching messages: \(error)")
 				return
 			}
 			guard let records = records else { return }
-			self.messages = records.flatMap { Message(cloudKitRecord: $0) }.sort { $0.date.compare($1.date) == NSComparisonResult.OrderedAscending }
+			
+			self.messages = records.flatMap { Message(cloudKitRecord: $0) }
 		}
 	}
 	
 	func subscribeForPushNotifications(completion: ((NSError?) -> Void)? = nil) {
-		cloudKitManager.subscribe(Message.recordType,
-		                          predicate: NSPredicate(value: true),
-		                          subscriptionID: "MessagesSubscription",
-		                          contentAvailable: false,
-		                          alertBody: "There's a new message on the bulletin board!",
-		                          options: .FiresOnRecordCreation) { (subscription, error) in
+		
+		cloudKitManager.subscribeToCreationOfRecordsWithType(Message.recordType) { (error) in
 			if let error = error {
 				NSLog("Error saving subscription: \(error)")
 			} else {
@@ -75,7 +70,7 @@ public class MessagesController: NSObject {
 	
 	private(set) var messages = [Message]() {
 		didSet {
-			dispatch_async(dispatch_get_main_queue()) { 
+			dispatch_async(dispatch_get_main_queue()) {
 				let nc = NSNotificationCenter.defaultCenter()
 				nc.postNotificationName(MessagesControllerDidRefreshNotification, object: self)
 			}
