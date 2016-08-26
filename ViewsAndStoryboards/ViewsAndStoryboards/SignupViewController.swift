8c19858f7334c60d611e2f83e25d2c99fd034a7e
Andrew Madsen <andrew@openreelsoftware.com>
2016-08-26 11:26:49 -0600

    ViewsAndStoryboards: Code cleanup and project reorganization.

--- ViewsAndStoryboards/ViewsAndStoryboards/SignupViewController.swift
+++ ViewsAndStoryboards/ViewsAndStoryboards/SignupViewController.swift
@@ -9,21 +9,7 @@
 import UIKit
 
 class SignupViewController: UIViewController {
-
-    @IBOutlet weak var emailTextField: UITextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    
+	
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -34,5 +20,7 @@ class SignupViewController: UIViewController {
         }
     }
     
-
+	// MARK: - Properties
+	
+	@IBOutlet weak var emailTextField: UITextField!
 }
