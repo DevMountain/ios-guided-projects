69f860490db6ab04b6882acbcbaf4f007d85ce3b
Andrew Madsen <andrew@openreelsoftware.com>
2016-08-30 10:58:35 -0600

    TableView: Remove (unused) Core Data code, fix serious code quality issues, etc.

--- TableView/TableViewMiniLesson/WeekTableViewController.swift
+++ TableView/TableViewMiniLesson/WeekTableViewController.swift
@@ -9,35 +9,30 @@
 import UIKit
 
 class WeekTableViewController: UITableViewController {
-    
-    var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cellID") as UITableViewCell?
-        cell?.textLabel?.text = daysOfWeek[indexPath.row]
-        return cell!
-    }
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return daysOfWeek.count
-    }
-    
-    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 135
-    }
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "detailSegue",
-            let indexPath : NSIndexPath = tableView.indexPathForSelectedRow {
-            let detailViewController  = segue.destinationViewController as? DetailViewController
-            
-            detailViewController?.titleString = daysOfWeek[indexPath.row]
-        }
-    }
-    
+	
+	var daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+	
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("DayCell", forIndexPath: indexPath)
+		cell.textLabel?.text = daysOfWeek[indexPath.row]
+		return cell
+	}
+	
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return daysOfWeek.count
+	}
+	
+	override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+		return 135
+	}
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "DetailSegue" {
+			guard let indexPath = tableView.indexPathForSelectedRow else { return }
+			let day = daysOfWeek[indexPath.row]
+			
+			let detailViewController = segue.destinationViewController
+			detailViewController.title = day
+		}
+	}
 }
