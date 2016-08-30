475dd433d968d36016753b226fbf419f2e5517fd
Andrew Madsen <andrew@openreelsoftware.com>
2016-08-30 12:01:39 -0600

    Converts PlaylistNSUserDefaults to Swift 3.

--- PlaylistNSUserDefaults/PlaylistNSUserDefaults/PlaylistTableViewController.swift
+++ PlaylistNSUserDefaults/PlaylistNSUserDefaults/PlaylistTableViewController.swift
@@ -10,57 +10,57 @@ import UIKit
 
 class PlaylistTableViewController: UITableViewController {
 	
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        tableView.reloadData()
-    }
-
-    @IBAction func addButtonTapped(sender: AnyObject) {
-        guard let playlist = playlistTextField.text where playlist.characters.count > 0 else {return}
-        PlaylistController.sharedController.addPlaylist(playlist)
-        playlistTextField.text = ""
-        tableView.reloadData()
-    }
-    
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		tableView.reloadData()
+	}
+	
+	@IBAction func addButtonTapped(_ sender: AnyObject) {
+		guard let playlistName = playlistTextField.text, !playlistName.isEmpty else {return}
+		PlaylistController.sharedController.add(playlistWithName: playlistName)
+		playlistTextField.text = ""
+		tableView.reloadData()
+	}
+	
 	// MARK: UITableViewDataSource/Delegate
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return PlaylistController.sharedController.playlists.count
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("playlistCell", forIndexPath: indexPath)
-
-        let playlist = PlaylistController.sharedController.playlists[indexPath.row]
-        cell.textLabel?.text = playlist.name
-        cell.detailTextLabel?.text = "\(playlist.songs.count) songs"
-
-        return cell
-    }
-
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Playlists"
-    }
-    
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            let playlist = PlaylistController.sharedController.playlists[indexPath.row]
-            PlaylistController.sharedController.deletePlaylist(playlist)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        }
-    }
-
-    // MARK: Navigation
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "toPlaylistDetail",
-            let indexPath = tableView.indexPathForSelectedRow {
-            let playlist = PlaylistController.sharedController.playlists[indexPath.row]
-            let songVC = segue.destinationViewController as? SongTableViewController
-            songVC?.playlist = playlist
-        }
-    }
-
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return PlaylistController.sharedController.playlists.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "playlistCell", for: indexPath)
+		
+		let playlist = PlaylistController.sharedController.playlists[indexPath.row]
+		cell.textLabel?.text = playlist.name
+		cell.detailTextLabel?.text = "\(playlist.songs.count) songs"
+		
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return "Playlists"
+	}
+	
+	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
+			let playlist = PlaylistController.sharedController.playlists[indexPath.row]
+			PlaylistController.sharedController.delete(playlist: playlist)
+			tableView.deleteRows(at: [indexPath], with: .fade)
+		}
+	}
+	
+	// MARK: Navigation
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == "toPlaylistDetail",
+			let indexPath = tableView.indexPathForSelectedRow {
+			let playlist = PlaylistController.sharedController.playlists[indexPath.row]
+			let songVC = segue.destination as? SongTableViewController
+			songVC?.playlist = playlist
+		}
+	}
+	
 	// MARK: Properties
 	
 	@IBOutlet weak var playlistTextField: UITextField!
