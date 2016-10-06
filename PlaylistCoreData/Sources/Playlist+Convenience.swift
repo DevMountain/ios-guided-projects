e5837b404b581eacb5250a4d85d6314ab9bc9c40
Andrew Madsen <andrew@openreelsoftware.com>
2016-10-06 11:49:14 -0600

    PlaylistCoreData: Switch to using automatic code generation for NSManagedObject subclasses.

--- PlaylistCoreData/Sources/Playlist+Convenience.swift
+++ PlaylistCoreData/Sources/Playlist+Convenience.swift
@@ -1,18 +1,17 @@
 //
-//  Playlist+CoreDataClass.swift
+//  Playlist+Convenience.swift
 //  PlaylistCoreData
 //
-//  Created by Andrew Madsen on 9/29/16.
+//  Created by Andrew Madsen on 10/6/16.
 //  Copyright © 2016 DevMountain. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-public class Playlist: NSManagedObject {
-	
+extension Playlist {
 	@discardableResult convenience init?(name: String,
-	                  context: NSManagedObjectContext = Stack.context) {
+	                                     context: NSManagedObjectContext = Stack.context) {
 		self.init(context: context)
 		self.name = name
 	}
