0b15f9cdcd90c0ffcbecc4c27026cca36634a3a1
Spencer Curtis <spencercolecurtis@gmail.com>
2017-03-13 14:19:04 -0600

    Add CollectionViews Project Start

---
+++ CollectionViews/CollectionViews/StateHelper.swift
@@ -0,0 +1,15 @@
+//
+//  States.swift
+//  CollectionViews
+//
+//  Created by Spencer Curtis on 2/25/17.
+//  Copyright © 2017 Spencer Curtis. All rights reserved.
+//
+
+import Foundation
+
+enum StateHelper {
+    static var abbreviations = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    
+    static var fullNames = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia","Washington", "West Virginia", "Wisconsin", "Wyoming"]
+}
