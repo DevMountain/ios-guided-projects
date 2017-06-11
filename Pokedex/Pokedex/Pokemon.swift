35c4c7ea4b745af6733a159dd8936667bfc9f641
Spencer Curtis <spencercolecurtis@gmail.com>
2017-06-11 14:46:17 -0600

    Update Pokedex project to follow format of other guided projects
    Change Pokemon struct to class
    Remove NetworkController, and use URLSession directly in PokemonController
    Clean up ViewController

--- Pokedex/Pokedex/Pokemon.swift
+++ Pokedex/Pokedex/Pokemon.swift
@@ -8,22 +8,45 @@
 
 import Foundation
 
-struct Pokemon {
-	let name: String
-	let id: Int
-	let abilities: [String]
+class Pokemon {
+    
+    private let nameKey = "name"
+    private let idKey = "id"
+    private let abilitiesKey = "abilities"
+    private let abilityKey = "ability"
+    private let abilityNameKey = "name"
+    
+    init(name: String, id: Int, abilities: [String]) {
+        self.name = name
+        self.id = id
+        self.abilities = abilities
+    }
+    
+    init?(dictionary: [String:AnyObject]) {
+        guard let name = dictionary[nameKey] as? String,
+            let id = dictionary[idKey] as? Int,
+            let abilityDictionaries = dictionary[abilitiesKey] as? [[String:AnyObject]] else {
+                return nil
+        }
+        
+        var abilities: [String] = []
+        
+        for abilityDictionary in abilityDictionaries {
+            
+            guard let abilityNameDictionary = abilityDictionary[abilityKey] as? [String: String],
+                let ability = abilityNameDictionary[abilityNameKey] else { break }
+            
+            abilities.append(ability)
+        }
+        
+        self.name = name
+        self.id = id
+        self.abilities = abilities
+        
+    }
+    
+    let name: String
+    let id: Int
+    let abilities: [String]
 }
 
-extension Pokemon {
-	init?(dictionary: [String:AnyObject]) {
-		guard let name = dictionary["name"] as? String,
-			let id = dictionary["id"] as? Int,
-			let abilityDictionaries = dictionary["abilities"] as? [[String:AnyObject]] else {
-				return nil
-		}
-		
-		let abilities = abilityDictionaries.flatMap { $0["ability"]?["name"] as? String }
-		
-		self.init(name: name, id: id, abilities: abilities)
-	}
-}
