d6ba20409f9042ca8595b69b4f1385e1a9fa6759
Spencer Curtis <spencercolecurtis@gmail.com>
2017-06-11 16:42:39 -0600

    Change Card from class back to struct

--- DeckOfOneCard/DeckOfOneCard/Card.swift
+++ DeckOfOneCard/DeckOfOneCard/Card.swift
@@ -8,31 +8,27 @@
 
 import Foundation
 
-class Card {
+struct Card {
     
-    private let suitKey = "suit"
-    private let valueKey = "value"
-    private let imageKey = "image"
-	
-    init(imageEndpoint: String, value: String, suit: String) {
-        self.imageEndpoint = imageEndpoint
-        self.value = value
-        self.suit = suit
-    }
+    let imageEndpoint: String
+    let value: String
+    let suit: String
+}
+
+extension Card {
+    
+    private static var suitKey: String { return "suit" }
+    private static var valueKey: String { return "value" }
+    private static var imageKey: String { return "image" }
     
     init?(dictionary: [String: Any]) {
-        guard let imageEndpoint = dictionary[imageKey] as? String,
-            let value = dictionary[valueKey] as? String,
-            let suit = dictionary[suitKey] as? String else { return nil }
+        guard let imageEndpoint = dictionary[Card.imageKey] as? String,
+            let value = dictionary[Card.valueKey] as? String,
+            let suit = dictionary[Card.suitKey] as? String else { return nil }
         
         self.imageEndpoint = imageEndpoint
         self.value = value
         self.suit = suit
     }
-	
-	// MARK: Properties
-	
-	let imageEndpoint: String
-	let value: String
-	let suit: String
+    
 }
